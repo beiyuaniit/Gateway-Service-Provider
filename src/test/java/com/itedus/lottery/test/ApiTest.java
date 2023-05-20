6c1ed177b747f723bb732ca8ae1df6eab8129854
Local <beiyuan@me.com>
2023-05-20 22:17:38 +0800

    common commit :打点补丁，现在能直接用

---
+++ src/test/java/com/itedus/lottery/test/ApiTest.java
@@ -0,0 +1,11 @@
+package com.itedus.lottery.test;
+
+import com.alibaba.fastjson.JSON;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ApiTest {
+
+    private Logger logger = LoggerFactory.getLogger(ApiTest.class);
+
+}
